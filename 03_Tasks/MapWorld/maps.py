33175f17a25b7c98bcf689043f16a040adef920e
JacobLoe <40402197+JacobLoe@users.noreply.github.com>
2021-07-22 15:28:15 +0200

    Update maps.py

--- 03_Tasks/MapWorld/maps.py
+++ 03_Tasks/MapWorld/maps.py
@@ -189,15 +189,12 @@ class ADEMap(AbstractMap):
                                         len(target_type_distr), replace=False)
         for target_type, repetitions in zip(target_types, target_type_distr):
             for _ in range(repetitions):
-                try:
+                if len(unassigned) > 0:
                     this_node = unassigned[np.random.choice(range(len(unassigned)))]
                     G.nodes[this_node]['base_type'] = 'indoor'
                     G.nodes[this_node]['type'] = target_type
                     G.nodes[this_node]['target'] = True
                     unassigned.remove(this_node)
-                except:
-                    # skip the room type assignment if no unassigned rooms are left
-                    pass
         remainder_types = list(set(self._target_cats)
                                .difference(set(target_types))
                                .union(set(self._distractor_cats)))
